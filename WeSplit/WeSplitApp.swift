a0ce51f9c7ede86a127086538a9d60b8e6a8b7c8
Djroton Noé Sossou <70499809+noahdjroton@users.noreply.github.com>
2024-01-19 22:01:20 +0100

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Djroton Noé SOSSOU on 19/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
